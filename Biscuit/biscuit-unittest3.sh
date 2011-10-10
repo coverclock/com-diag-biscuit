4cef057aedf3fddbafe1484064c857ef52aae236
jsloan <jsloan@0de1f139-0e01-4bdb-9df9-57a33ae264fd>
2011-10-10 19:16:42 +0000

    CHeckpoint after a successful test of unittest3.
    git-svn-id: svn://graphite/biscuit/trunk@7 0de1f139-0e01-4bdb-9df9-57a33ae264fd

--- Biscuit/biscuit-unittest3.sh
+++ Biscuit/biscuit-unittest3.sh
@@ -12,12 +12,10 @@ WORKING="`pwd`"
 echo "${NAME} PATH=\"${PATH}\"" 1>&2
 echo "${NAME} LD_LIBRARY_PATH=\"${LD_LIBRARY_PATH}\"" 1>&2
 echo "${NAME} HOME=\"${HOME}\"" 1>&2
-ls -lR ${HOME} 1>&2
 echo "${NAME} SOURCE=\"${SOURCE}\"" 1>&2
-ls -lR ${SOURCE} 1>&2
 echo "${NAME} WORKING=\"${WORKING}\"" 1>&2
-ls -lR ${WORKING} 1>&2
-rm -f ${WORKING}/biscuit-unittest.dat
-[ -f ${SOURCE}/millay.txt ] || exit 1
-touch ${WORKING}/biscuit-unittest.dat
+rm -f ${WORKING}/biscuit-unittest3a.dat
+[ -f ${SOURCE}/biscuit-unittest3a.txt ] && touch ${WORKING}/biscuit-unittest3a.dat
+rm -f ${WORKING}/biscuit-unittest3b.dat
+[ -f ${SOURCE}/subdir/biscuit-unittest3b.txt ] && touch ${WORKING}/biscuit-unittest3b.dat
 exit 0
