895423a79cd06741b9306b70dda735afea093989
jsloan <jsloan@0de1f139-0e01-4bdb-9df9-57a33ae264fd>
2011-10-11 15:33:53 +0000

    Rearranged artifact directory to meet new multi-platform scheme. Unit tests 1, 2 and 3 all succeed.
    git-svn-id: svn://graphite/biscuit/trunk/Biscuit@12 0de1f139-0e01-4bdb-9df9-57a33ae264fd

--- biscuit-syslog.sh
+++ biscuit-syslog.sh
@@ -5,3 +5,17 @@
 # Chip Overclock <coverclock@diag.com>
 # http://www.diag.com/navigation/downloads/Biscuit
 ################################################################################
+NAME="`basename $0`"
+SOURCE="`dirname $0`"
+[ -z "${SOURCE}" ] && SOURCE="."
+WORKING="`pwd`"
+echo "${NAME}: PATH=\"${PATH}\"" 1>&2
+echo "${NAME}: LD_LIBRARY_PATH=\"${LD_LIBRARY_PATH}\"" 1>&2
+echo "${NAME}: HOME=\"${HOME}\"" 1>&2
+echo "${NAME}: SOURCE=\"${SOURCE}\"" 1>&2
+echo "${NAME}: WORKING=\"${WORKING}\"" 1>&2
+rm -f ${WORKING}/biscuit-unittest3a.dat
+[ -f ${SOURCE}/biscuit-unittest3a.txt ] && touch ${WORKING}/biscuit-unittest3a.dat || exit 1
+rm -f ${WORKING}/biscuit-unittest3b.dat
+[ -f ${SOURCE}/subdir/biscuit-unittest3b.txt ] && touch ${WORKING}/biscuit-unittest3b.dat || exit 2
+exit 0
