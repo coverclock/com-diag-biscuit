8bb33b15a3812c8c94a988fd22609f21de815809
jsloan <jsloan@0de1f139-0e01-4bdb-9df9-57a33ae264fd>
2011-10-12 17:37:54 +0000

    Checkpoint after a successful multi-platform execution of unittests 1 through 3 for cascada and silver.
    git-svn-id: svn://graphite/biscuit/trunk@13 0de1f139-0e01-4bdb-9df9-57a33ae264fd

---
+++ Biscuit/hotplug.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+################################################################################
+# Copyright 2011 by the Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in README.h
+# Chip Overclock <coverclock@diag.com>
+# http://www.diag.com/navigation/downloads/Biscuit
+# Install (if /sbin/hotplug and /etc/hotplug.d doesn't already exist):
+#  cp hotplug.sh /sbin/hotplug
+#  chmod 755 /sbin/hotplug
+#  mkdir -p /etc/hotplug.d/block
+################################################################################
+DIR="/etc/hotplug.d"
+for FF in ${DIR}/$1/*.hotplug ${DIR}/default/*.hotplug; do
+    if [ -x ${FF} ]; then
+        ${FF} $1
+    fi
+done
+exit 0
